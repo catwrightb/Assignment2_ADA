6cfb4364239a356fccc1f9f9e5664fbe21cea360
catwrightb <78883365+catwrightb@users.noreply.github.com>
2021-08-30 01:41:17 +1200

    Polygon is created with a passed exterior edges number (n). Then random points are chosen with Java Random using triangular angle formulas. These are then sorted via getting the centroid coordinates and the calculating the degrees between two points to sort the points on the degree.

--- src/Demo.java
+++ src/Demo.java
@@ -1,18 +1,14 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 
-public class CustomDrawingStartCode extends JPanel{
+public class Demo extends JPanel{
     public final int PANEL_WIDTH = 500;
     public final int PANEL_HEIGHT = 500;
     //private DrawingPanel drawPanel;  //innerclass
     private String[] values = new String[] {"4", "5", "6", "7", "8", "9"};
 
 
-    public CustomDrawingStartCode() {
+    public Demo() {
         super(new BorderLayout());   //invoke super class Jpanel constructor use BorderLayout
         //drawPanel = new DrawingPanel();     //create DrawingPanel and add to center
         //add(drawPanel, BorderLayout.CENTER);
@@ -29,30 +25,12 @@ public class CustomDrawingStartCode extends JPanel{
         setBackground(Color.PINK);
 
         add(topPanel, BorderLayout.NORTH);
-        add(new Shape(PANEL_WIDTH, PANEL_HEIGHT), BorderLayout.CENTER);
+        add(new Shape(PANEL_WIDTH, PANEL_HEIGHT, 6), BorderLayout.CENTER);
         add(bottomPanel, BorderLayout.SOUTH);
     }
 
-//    //seperate inner class for graphics
-//    private class DrawingPanel extends JPanel
-//    {   public DrawingPanel()
-//    {   setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
-//        setBackground(Color.WHITE);
-//    }
-//        @Override
-//        public void paintComponent(Graphics g){
-//            super.paintComponent(g);
-//            g.setColor(Color.BLACK);  //dip paint brush in black
-//            g.fillRect(0, 0, 200,100);//top left corner coordinate,width & height
-//            g.setColor(Color.YELLOW); //dip paint brush in yellow
-//            g.drawOval(0,0,100,100);//top left corner coordinate,width & height
-//            g.fillOval(100,0,100,100);
-//
-//            //can do all drawing here using Graphics object g.
-//        }
-//    }
     public static void main(String[] args) {
-        CustomDrawingStartCode myPanel = new CustomDrawingStartCode();
+        Demo myPanel = new Demo();
         JFrame frame = new JFrame("Custom Drawing"); //create frame to hold our JPanel subclass
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(myPanel);  //add instance of MyGUI to the frame
--- src/Shape.java
+++ src/Shape.java
@@ -1,14 +1,23 @@
+import javafx.scene.shape.Circle;
+
 import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Random;
 
 public class Shape extends Component {
 
     int width;
     int height;
+    int n;
 
-    public Shape(int PANEL_WIDTH, int PANEL_HEIGHT) {
+    public Shape(int PANEL_WIDTH, int PANEL_HEIGHT, int sides) {
         setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
         width = PANEL_WIDTH;
         height = PANEL_HEIGHT;
+        n = sides;
         //setBackground(Color.GREEN);
 
     }
@@ -16,41 +25,72 @@ public class Shape extends Component {
     public void paint(Graphics g) {
 
         // Retrieve the graphics context; this object is used to paint shapes
-
         Graphics2D g2d = (Graphics2D) g;
 
-        // Draw an oval that fills the window
+        int radius = 100;
+        int x = width/2;
+        int y = height/2;
 
-        int min = 50;
-        int max = 300;
-        int c = (int)(Math.random()*(max-min+1)+min);
+        ArrayList<Coordinate> coordinates = new ArrayList<Coordinate>();
+        //draws circle
+        g.setColor(Color.MAGENTA);
+        g.drawOval(x-radius, y-radius, 2*radius, 2*radius);
 
-        int x = 0;
-        int y = 0;
-        int w = getSize().width - 1;
-        int h = getSize().height - 1;
+        //point at center of circle
+        g.fillOval(x, y, 2,2);
 
+        int m = Math.min(x, y);
+        int c = 4 * m / 5;
+        //creates radius for points on the circle
+        int r2 = Math.abs(m - c) / 9;
+        Random random = new Random();
+
+        for (int i = 0; i < n; i++) {
+            //triangulates the points on the circle
+           //double t = 2 * Math.PI * i / n;
+            double t = random.nextDouble() * Math.PI*2;
+            int a = (int) Math.round(x + radius *Math.cos(t));
+            int b = (int) Math.round(y + radius *Math.sin(t));
+            g.setColor(Color.BLACK);
+            g.fillOval(a-r2, b-r2, 2*r2, 2*r2);
+            coordinates.add(new Coordinate(a,b));
+        }
+
+        sortVerticies(coordinates);
 
         Polygon polygon = new Polygon();
 
-        int a = (width/2)-50;
-        int b= (height/2)-50;
-        int z =100;
-        int p = 75;
+        for (int i = 0; i < coordinates.size(); i++) {
+            polygon.addPoint(coordinates.get(i).x, coordinates.get(i).y);
+        }
 
-        polygon.addPoint(a, b);
-        polygon.addPoint(a+z, b);
-
-        polygon.addPoint(a+p, b+z);
-        polygon.addPoint(a, b+z);
-
-
+        g2d.setColor(Color.RED);
         g2d.drawPolygon(polygon);
-
-        g.setColor(Color.blue);
-        g.drawLine(a,b,a+z,b+z);
 
     }
 
+    public static Point findCentroid(ArrayList<Coordinate> points) {
+        int x = 0;
+        int y = 0;
+        for (Coordinate p : points) {
+            x += p.x;
+            y += p.y;
+        }
+        Point center = new Point(0, 0);
+        center.x = x / points.size();
+        center.y = y / points.size();
+        return center;
+    }
+
+    public static ArrayList<Coordinate> sortVerticies(ArrayList<Coordinate> points) {
+        // get centroid
+        Point center = findCentroid(points);
+        Collections.sort(points, (a, b) -> {
+            double a1 = (Math.toDegrees(Math.atan2(a.x - center.x, a.y - center.y)) + 360) % 360;
+            double a2 = (Math.toDegrees(Math.atan2(b.x - center.x, b.y - center.y)) + 360) % 360;
+            return (int) (a1 - a2);
+        });
+        return points;
+    }
 
 }
