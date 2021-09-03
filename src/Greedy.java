baa8b2d1f8ea761e2cbab8ef0f8ff14f39b11eb2
Nyxxi <48755627+Nyxxi@users.noreply.github.com>
2021-09-03 15:41:56 +1200

    minor changes again

--- src/Greedy.java
+++ src/Greedy.java
@@ -1,4 +1,3 @@
-
 import java.awt.geom.Line2D;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -25,6 +24,7 @@ public class Greedy {
             greedyLines.add(temp);
 
             //checks first if the points are the same then if they are not the same checks if there is an intersection
+            //and removes the line if an intersection is found.
             lineList.removeIf(line -> !(temp.x == line.x && temp.y == line.y
                     || temp.x2 == line.x2 && temp.y2 == line.y2
                     || temp.x == line.x2 && temp.y == line.y2
