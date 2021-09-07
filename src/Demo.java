51a01f8d4d9a10525553f34925621c00dba56b37
catwrightb <78883365+catwrightb@users.noreply.github.com>
2021-09-07 22:45:55 +1200

    Gui working now

--- src/Demo.java
+++ src/Demo.java
@@ -1,39 +1,26 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
 import javax.swing.*;
 
-public class Demo extends JPanel{
-    public final int PANEL_WIDTH = 500;
-    public final int PANEL_HEIGHT = 500;
+public class Demo extends JFrame{
+    public static final int PANEL_WIDTH = 500;
+    public static final int PANEL_HEIGHT = 500;
     //private DrawingPanel drawPanel;  //innerclass
-    private String[] values = new String[] {"4", "5", "6", "7", "8", "9"};
 
 
-    public Demo() {
-        super(new BorderLayout());   //invoke super class Jpanel constructor use BorderLayout
-        //drawPanel = new DrawingPanel();     //create DrawingPanel and add to center
-        //add(drawPanel, BorderLayout.CENTER);
-        JPanel topPanel = new JPanel();
-        JComboBox<String> testCombo = new JComboBox<String>(values);
-        JButton submitButton = new JButton("Submit");
-        topPanel.add(testCombo);
-        topPanel.add(submitButton);
-        setBackground(Color.GREEN);
 
-        JPanel bottomPanel = new JPanel();
-        JButton clearButton = new JButton("Clear");
-        bottomPanel.add(clearButton, BorderLayout.CENTER);
-        setBackground(Color.PINK);
+    public Demo(int sides) {
 
-        add(topPanel, BorderLayout.NORTH);
-        add(new Shape(PANEL_WIDTH, PANEL_HEIGHT, 6), BorderLayout.CENTER);
-        add(bottomPanel, BorderLayout.SOUTH);
     }
 
+
     public static void main(String[] args) {
-        Demo myPanel = new Demo();
-        JFrame frame = new JFrame("Custom Drawing"); //create frame to hold our JPanel subclass
+        JFrame frame = new JFrame("Polygon Generator"); //create frame to hold our JPanel subclass
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().add(myPanel);  //add instance of MyGUI to the frame
+        frame.setPreferredSize(new Dimension(500,500) );
+        frame.getContentPane().add(new Shape(PANEL_WIDTH, PANEL_HEIGHT));  //add instance of MyGUI to the frame
         frame.pack(); //resize frame to fit our Jpanel
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
         frame.setLocation(new Point((d.width / 2) - (frame.getWidth() / 2), (d.height / 2) - (frame.getHeight() / 2)));
@@ -42,5 +29,6 @@ public class Demo extends JPanel{
         frame.setResizable(false);
 
     }
+
 
 }
--- src/Shape.java
+++ src/Shape.java
@@ -1,95 +1,157 @@
+import javafx.scene.control.Alert;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Objects;
 import java.util.Random;
 
-//TODO need to fix brute force to use the GetInteriorAngle class
-//TODO create gui display to show the paths of different approaches, maybe time comparison too
+
 
 public class Shape extends JPanel {
 
     private int width;
     private int height;
     private int n;
+    private String[] values = new String[] {"4", "5", "6", "7", "8", "9"};
+    int number = 0;
+    boolean clicked = false;
 
 
-
-    public Shape(int PANEL_WIDTH, int PANEL_HEIGHT, int sides) {
+    public Shape(int PANEL_WIDTH, int PANEL_HEIGHT) {
+        super(new BorderLayout());   //invoke super class Jpanel constructor use BorderLayout
         setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
+
+        JPanel topPanel = new JPanel();
+        JComboBox<String> testCombo = new JComboBox<String>(values);
+        this.setSize(PANEL_WIDTH,PANEL_HEIGHT);
+
+        testCombo.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                String numberOfSides =  Objects.requireNonNull(testCombo.getSelectedItem()).toString();
+
+                number = Integer.parseInt(numberOfSides);
+                System.out.println(number);
+                n = number;
+            }
+        });
+
+        JButton submitButton = new JButton("Submit");
+
+        submitButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                if (number != 0){
+                    clicked = true;
+                    repaint();
+                }
+                else {
+                    JOptionPane.showMessageDialog(null,"Please select a number of sides option",
+                            "Number of sides is 0!", JOptionPane.ERROR_MESSAGE);
+                }
+
+                //System.out.println("test");
+            }
+        });
+
+        topPanel.add(testCombo);
+        topPanel.add(submitButton);
+
+        JPanel bottomPanel = new JPanel();
+        JButton clearButton = new JButton("Clear");
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                number = 0;
+                clicked = false;
+                repaint();
+            }
+        });
+        bottomPanel.add(clearButton, BorderLayout.CENTER);
+
+        add(topPanel, BorderLayout.NORTH);
+        //add(new Shape(PANEL_WIDTH, PANEL_HEIGHT, sides), BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.SOUTH);
+
         width = PANEL_WIDTH;
         height = PANEL_HEIGHT;
-        n = sides;
 
     }
 
+
+    //paintComponent will paint 2 shapes fyi only one is seen though
+    // this is something we cant control
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        //System.out.println("-----------------------");
+        System.out.println("Paint");
         // Retrieve the graphics context; this object is used to paint shapes
         Graphics2D g2d = (Graphics2D) g;
 
-        int radius = 150;
-        int x = width/2;
-        int y = height/2;
+        if (clicked) {
 
-        //draws circle to check points are along the circle diamemter
-        g.setColor(Color.MAGENTA);
-        g.drawOval(x-radius, y-radius, 2*radius, 2*radius);
+            int radius = 150;
+            int x = width / 2;
+            int y = height / 2;
 
-       // point at center of circle
-        g.fillOval(x, y, 2,2);
+            //draws circle to check points are along the circle diamemter
+            g.setColor(Color.MAGENTA);
+            g.drawOval(x - radius, y - radius, 2 * radius, 2 * radius);
 
-        //gathers coordinates for polygon
-        ArrayList<Coordinate> coordinates = new ArrayList<Coordinate>();
-        //back up list that matches the coordinate list
-        ArrayList<Coordinate> points = new ArrayList<Coordinate>();
-        //collects the distances of the interior edges
-        ArrayList<CoordinateWithDistance> distanceBetweenPoints = new ArrayList<>();
+            // point at center of circle
+            g.fillOval(x, y, 2, 2);
 
-        ArrayList<Triangle> triangleslist = new ArrayList<>();
+            //gathers coordinates for polygon
+            ArrayList<Coordinate> coordinates = new ArrayList<Coordinate>();
+            //back up list that matches the coordinate list
+            ArrayList<Coordinate> points = new ArrayList<Coordinate>();
+            //collects the distances of the interior edges
+            ArrayList<CoordinateWithDistance> distanceBetweenPoints = new ArrayList<>();
+
+            ArrayList<Triangle> triangleslist = new ArrayList<>();
 
 
-        int m = Math.min(x, y);
-        int c = 4 * m / 5;
-        //creates radius for points on the circle
-        int r2 = Math.abs(m - c) / 9;
-        Random random = new Random();
+            int m = Math.min(x, y);
+            int c = 4 * m / 5;
+            //creates radius for points on the circle
+            int r2 = Math.abs(m - c) / 9;
+            Random random = new Random();
 
-        for (int i = 0; i < n; i++) {
-            //triangulates the points on the circle
-           //double t = 2 * Math.PI * i / n;
-            double t = random.nextDouble() * Math.PI*2;
-            int a = (int) Math.round(x + radius *Math.cos(t));
-            int b = (int) Math.round(y + radius *Math.sin(t));
-            g.setColor(Color.BLACK);
-            g.fillOval(a-r2, b-r2, 2*r2, 2*r2);
-            coordinates.add(new Coordinate(a,b));
-            points.add(new Coordinate(a,b));
+            for (int i = 0; i < n; i++) {
+                //triangulates the points on the circle
+                //double t = 2 * Math.PI * i / n;
+                double t = random.nextDouble() * Math.PI * 2;
+                int a = (int) Math.round(x + radius * Math.cos(t));
+                int b = (int) Math.round(y + radius * Math.sin(t));
+                g.setColor(Color.BLACK);
+                g.fillOval(a - r2, b - r2, 2 * r2, 2 * r2);
+                coordinates.add(new Coordinate(a, b));
+                points.add(new Coordinate(a, b));
+            }
+
+            sortVerticies(coordinates);
+            sortVerticies(points);
+
+            Polygon polygon = new Polygon();
+
+            //creates the exterior edges of the polygon
+            for (Coordinate coordinate : coordinates) {
+                polygon.addPoint(coordinate.x, coordinate.y);
+            }
+
+            g2d.setColor(Color.RED);
+            g2d.drawPolygon(polygon);
+
+            BruteForce bruteforce = new BruteForce(points);
+            triangleslist = bruteforce.startInteriorLineSearch();
+
+            drawLines(g, triangleslist);
+
+
         }
-
-        sortVerticies(coordinates);
-        sortVerticies(points);
-
-        Polygon polygon = new Polygon();
-
-        //creates the exterior edges of the polygon
-        for (Coordinate coordinate : coordinates) {
-            polygon.addPoint(coordinate.x, coordinate.y);
-        }
-
-        g2d.setColor(Color.RED);
-        g2d.drawPolygon(polygon);
-
-        BruteForce bruteforce = new BruteForce(points);
-        triangleslist = bruteforce.startInteriorLineSearch();
-
-        drawLines(g, triangleslist);
-
-
-
     }
 
     public static void drawLines(Graphics g, ArrayList<Triangle> triangleslist){
